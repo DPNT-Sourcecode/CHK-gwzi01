8c9519a56b1b865248e6d66ab77efb673fe5c06e
sbx_user1051 <sbx_user1051@169.254.106.241>
2022-03-14 13:06:05 +0000

    Mon Mar 14 13:06:05 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,8 @@ import re
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
+    # Could do with knowing what format the input is, unless this is deliberate! :)
+
+    num_A = re.search()
+
 
