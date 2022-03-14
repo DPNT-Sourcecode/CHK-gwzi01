090f946d423e49f2bac02d310bb8b562e0b03a85
sbx_user1051 <sbx_user1051@169.254.106.241>
2022-03-14 12:44:37 +0000

    Mon Mar 14 12:44:37 UTC 2022

--- lib/solutions/HLO/tests.py
+++ lib/solutions/HLO/tests.py
@@ -3,6 +3,12 @@ import unittest
 from .hello_solution import hello
 
 class TestHelloWorld(unittest.Testcase):
-    inputs_and_outputs = [
+    def test_hello_world(self):
+        inputs_and_outputs = [
+            ("John", "John says Hello world!"),
+            ("Dave", "Dave says Hello world!"),
+            (2, "A string must be passed to this function")
+        ]
 
-    ]
+        for case in inputs_and_outputs:
+            self.assertEqual(hello(case[0]), case[1])
