940cb9f1ec15fd321b5c474615bfe1405a6c8a0c
sbx_user1051 <sbx_user1051@169.254.106.241>
2022-03-14 15:00:51 +0000

    Mon Mar 14 15:00:51 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -74,6 +74,7 @@ def checkout(skus):
         total_cost += product_cost
 
     total_cost += calculate_B(skus.count("B"), skus.count("E"))
+    total_cost += calculate_B(skus.count("B"), skus.count("E"))
 
     #print(skus)
     #print(cost_A, cost_B, cost_C, cost_D, cost_E, cost_F)
@@ -134,6 +135,26 @@ def calculate_M(num_M, num_N):
 
     if num_N >= 3:
         free_M = num_M - int(num_M / 3)
+
+    num_M = num_M - free_M
+
+    return num_M * 15
+
+
+def calculate_Q(num_Q, num_R):
+
+    if num_Q == 0:
+        return 0
+    
+    free_Q = 0
+    
+    if num_R >= 3:
+        free_Q = int(num_R / 3)
+
+    num_Q = num_Q - free_Q
+
+    return (int(num_Q / 3) * 80) + (num_Q % 3 * 30)
+
 
 
 
