f96bb06b3ee3f65b9a7f1ef74fc23e132c13bdce
sbx_user1051 <sbx_user1051@169.254.106.241>
2022-03-14 12:47:37 +0000

    Mon Mar 14 12:47:37 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,6 +3,9 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    
+    if not isinstance(friend_name, str):
+        raise TypeError
     try:
         return "{} says Hello World!".format(friend_name)
     except TypeError:
--- lib/solutions/HLO/tests.py
+++ lib/solutions/HLO/tests.py
@@ -1,14 +1,17 @@
 import unittest
 
-from .hello_solution import hello
+from hello_solution import hello
 
-class TestHelloWorld(unittest.Testcase):
+class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         inputs_and_outputs = [
-            ("John", "John says Hello world!"),
-            ("Dave", "Dave says Hello world!"),
+            ("John", "John says Hello World!"),
+            ("Dave", "Dave says Hello World!"),
             (2, "A string must be passed to this function")
         ]
 
         for case in inputs_and_outputs:
             self.assertEqual(hello(case[0]), case[1])
+
+if __name__ == '__main__':
+    unittest.main()
