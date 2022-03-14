92a9d61249097e63912fd6fb6415b8c15f05231f
sbx_user1051 <sbx_user1051@169.254.106.241>
2022-03-14 13:24:05 +0000

    Mon Mar 14 13:24:05 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,7 @@ def checkout(skus):
     # Could do with knowing what format the input is, unless this is deliberate! :)
     # Assuming the input string is going to look something like AABBBBC etc
 
-    if not re.findall('[^A-D]', skus) or not re.findall('[A-D'):
+    if re.findall('[^A-D]', skus) or not re.findall('[A-D]', skus):
         print("invalid input")
         return -1
     
@@ -20,6 +20,8 @@ def checkout(skus):
     cost_C = 20 * num_C
     cost_D = 20 * num_D
     
+    return cost_A + cost_B + cost_C + cost_D
+
 
 
 
